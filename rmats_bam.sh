44dfa621d43b7bfb7d15563206391dd4c43c68db
Qiang Hu <qiang.hu@roswellpark.org>
2020-03-26 22:02:33 -0400

    fix bug

--- rmats_bam.sh
+++ rmats_bam.sh
@@ -5,4 +5,4 @@ b2=$2
 echo $b1 > /tmp/b1.txt
 echo $b2 > /tmp/b2.txt
 
-echo rmats.py --b1 /tmp/b1.txt --b2 /tmp/b2.txt -t $3 --readLength $4 --gtf $5 --od $6 --nthread $7 --tstat $8
+rmats.py --b1 /tmp/b1.txt --b2 /tmp/b2.txt -t $3 --readLength $4 --gtf $5 --od $6 --nthread $7 --tstat $8
